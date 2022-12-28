bf1dfc43d5a367a93f47699ae4f5c45dbe7f98cf
Ryan Punt <ryan@mirum.org>
2022-12-27 20:21:05 -0600

    initial

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/rpunt/snow
+
+go 1.19
+
+replace github.com/rpunt/dcc => ../dcc
+
+require github.com/rpunt/dcc v0.0.0-00010101000000-000000000000
---
+++ snow.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/rpunt/dcc"
+)
+
+
+func main() {
+	client := dcc.NewClient()
+	client.BaseURL = "https://icanhazdadjoke.com"
+	client.Headers["Accept"] = "application/json"
+	response, err := client.Get("/")
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+
+	fmt.Println("response:", response.Body)
+}
